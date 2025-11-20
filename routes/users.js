019b0b675d38d2b4435bb415cf8d97ebded3a583
Roan Jones <honestly2978@gmail.com>
2025-11-20 14:04:57 +0000

    Added username and password fields to form

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 // Create a new router
 const express = require("express")
-const router = express.Router()
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
@@ -8,6 +10,13 @@ router.get('/register', function (req, res, next) {
 
 router.post('/registered', function (req, res, next) {
     // saving data in database
+    const plainPassword = req.body.password;
+
+    bycrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        // store hashed password in database
+        
+    });
+
     res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                             
 }); 
 
